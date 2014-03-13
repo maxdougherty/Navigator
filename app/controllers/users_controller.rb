20bbb26efb85831e7f684fc51a272ce3037468f8
Max Dougherty <maxematical@gmail.com>
2014-03-13 13:32:50 -0700

    Added Bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,10 +14,12 @@ class UsersController < ApplicationController
 	def view_events
 		@user_events = current_user.events.order(start_time: :asc, end_time: :asc)
 
-		if @user_events.blank?
-			event_result = current_user.events.create(title: "Example Event!", start_time: 1200, end_time: 1400)
-			@user_events = current_user.events
-		end
+		# Uncomment to gives user at least one default event.
+
+		# if @user_events.blank?
+		# 	event_result = current_user.events.create(title: "Example Event!", start_time: 1200, end_time: 1400)
+		# 	@user_events = current_user.events
+		# end
 
 		render :view_events
 
@@ -86,4 +88,49 @@ class UsersController < ApplicationController
 		return
 	end
 
+	def json_get_user_events
+		@user_events = current_user.events.order(start_time: :asc, end_time: :asc)
+	end
+
+	def json_add_user_event
+		@errors = []
+
+		# Check to make sure no parameters are empty. If so, reject immediately
+		if params[:title].empty? || params[:start_time].empty? || params[:end_time].empty?
+			@errors.push("Invalid Input: Non-filled fields")
+			render :add_event
+			return
+		end
+		# Convert parameters to appropriate types
+		title = params[:title]
+		start_time = params[:start_time].to_i
+		end_time = params[:end_time].to_i
+
+		# Validate inputs
+		if (title.length > MAX_TITLE_LENGTH)
+			@errors.push("Invalid Title: More than 128 characters")
+		end
+		if (start_time < 0) || (end_time < 0)
+			@errors.push("Invalid Time: Seriously? Negative?")
+		end
+		if ((start_time % 100) % 60) != (start_time % 100) || (start_time % 2400) != start_time
+			@errors.push("Invalid Time: Start Time not correct format [hhmm]")
+		end
+		if ((end_time % 100) % 60) != (end_time % 100) || (end_time % 2400) != end_time
+			@errors.push("Invalid Time: End Time not correct format [hhmm]")
+		end
+
+		# If any errors occur, reject event creation and display errors
+		if not @errors.empty?
+			render :add_event
+			return
+		end
+
+		# Create new event through user to create association
+		current_user.events.create(title: title, start_time: start_time, end_time: end_time)
+	end
+
+	def json_delete_user_event
+
+	end
 end
