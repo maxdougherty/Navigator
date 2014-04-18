e88a3b911c4066b7672542afe1f629b2a68fcfa3
ycheon <c827jin@Yu-Jin-Cheons-MacBook-Pro.local>
2014-04-17 18:38:24 -0700

    added guitest

--- spec/GUITests.rb
+++ spec/GUITests.rb
@@ -1,12 +1,13 @@
 require "selenium-webdriver"
 browser = Selenium::WebDriver.for :chrome
-browser.get "http://dry-fortress-4431.herokuapp.com/"
+browser.get "http://mysterious-beyond-6236.herokuapp.com/"
 browser.find_element(link_text: "Sign up").click
 p browser.current_url
 p browser.title
 
-##sign up##
-browser.find_element(name: "user[email]").send_keys "user1000@schedule.com"
+##Sign up##
+browser.find_element(name: "user[email]").click
+browser.find_element(name: "user[email]").send_keys "user11@navigator.com"
 browser.find_element(name: "user[password]").send_keys "password"
 browser.find_element(name: "user[password_confirmation]").send_keys "password"
 browser.find_element(name: "commit").click
@@ -15,8 +16,6 @@ p browser.title
 
 ##create a schedule##
 browser.find_element(name: "title").send_keys "schedule1"
-browser.find_element(name: "start_time").send_keys "1200"
-browser.find_element(name: "end_time").send_keys "2200"
 browser.find_element(name: "commit").click
 p browser.current_url
 p browser.title
@@ -27,7 +26,18 @@ browser.find_element(name: "view").click
 browser.find_element(name: "title").send_keys "event1"
 browser.find_element(name: "start_time").send_keys "1200"
 browser.find_element(name: "end_time").send_keys "1400"
+browser.find_element(name: "duration").send_keys "100"
 browser.find_element(name: "address").send_keys "2020 Kittredge Ave. Berkeley, CA 94704"
+browser.find_element(name: "commit").click
+p browser.current_url
+p browser.title
+
+##create a second event##
+browser.find_element(name: "title").send_keys "event2"
+browser.find_element(name: "start_time").send_keys "1400"
+browser.find_element(name: "end_time").send_keys "1600"
+browser.find_element(name: "duration").send_keys "100"
+browser.find_element(name: "address").send_keys "201 Campbell Hall, Berkeley, CA 94704"
 browser.find_element(name: "commit").click
 p browser.current_url
 p browser.title
